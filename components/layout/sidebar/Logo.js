d34f1a19799950c90150bb7da32a90c636a34957
Žan Kogovšek <zk0821@student.uni-lj.si>
2023-05-18 16:24:14 +0200

    Handle Assets

--- components/layout/sidebar/Logo.js
+++ components/layout/sidebar/Logo.js
@@ -1,12 +1,18 @@
 /* eslint-disable @next/next/link-passhref */
 import Link from "next/link";
+import Image from "next/image";
 import React from "react";
 
 const Logo = () => {
     return (
         <Link href="/">
             <span className="inline-flex items-center justify-center h-20 w-full bg-cyan-300 hover:bg-cyan-400 focus:bg-cyan-400 cursor-pointer">
-                <img src="/logo.png" width="50"></img>
+                <Image
+                    src="/logo.png"
+                    alt="Fitness Stickman"
+                    height="50"
+                    width="50"
+                />
             </span>
         </Link>
     );
